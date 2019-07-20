719dd73f197896c871bb1afb3233615cdd8c3c94
Anand Ramasamy <48551515+AnandRamasamy3@users.noreply.github.com>
2019-07-20 08:51:24 +0530

    Update game3.py

--- source/package3/sources/game3/game3.py
+++ source/package3/sources/game3/game3.py
@@ -6,9 +6,12 @@ from random import *
 pygame.init()
 fps=20
 ft=pygame.time.Clock()
-curent_directory=getcwd().replace(chr(92),chr(47))
-temp_list=curent_directory.split(":",2)
-curent_directory=temp_list[0]+":/"+temp_list[1]
+try:
+    current_directory=os.getcwd().replace(chr(92),chr(47))
+    temp_list=current_directory.split(":",2)
+    current_directory=temp_list[0]+":/"+temp_list[1]
+except:
+    current_directory=os.getcwd()
 wallpaper_icon_big=pygame.image.load(curent_directory+"/source/package3/sources/game3/icons/game3_big.png")
 wallpaper_icon_small=pygame.image.load(curent_directory+"/source/package3/sources/game3/icons/game3_small.png")
 white=(255,255,255)
